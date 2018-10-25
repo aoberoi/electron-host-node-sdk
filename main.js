719df3939f0bf2cb6c38f20a91b5521ef07f7268
Ankur Oberoi <aoberoi@gmail.com>
2018-10-24 19:20:41 -0700

    import slack/client package and make a basic request

--- main.js
+++ main.js
@@ -1,2 +1,5 @@
-console.log('Running in electron node');
-console.log(process.version);
+const { WebClient } = require('@slack/client');
+
+const slack = new WebClient(process.env.SLACK_ACCESS_TOKEN);
+
+slack.auth.test().then(console.log).catch(console.error);
